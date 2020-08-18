fe95846b0ecbe0f50bfa12d87aeaad9ee58eca0f
Amy Carrillo <amycarrillo29@yahoo.com>
2020-08-17 20:41:54 -0500

    adding comments

--- lib/friendly_syntax.ex
+++ lib/friendly_syntax.ex
@@ -2,11 +2,14 @@ defmodule DIDemo.FriendlySyntax do
   @available_attribute "value"
 
   def i_am_public do
+    #function with arity 0 - meaning # of arguments
   end
 
   def i_am_public(name) do
+    # function with arity 1
   end
 
   defp i_am_private(name) do
+    # private function
   end
 end
